cd8d5448ccdc9eeff4d3bee6142d6bad23348343
mashaojie <mashaojie@pinefield-inc.com>
2023-08-11 21:33:35 +0800

    feat: 头像卡片代码编写

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-// import TWEEN from 'three/addons/libs/tween.module.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import html2canvas from 'html2canvas'
-// import { EXRLoader } from 'three/addons/loaders/EXRLoader.js';
-
-// import { FontLoader } from 'three/addons/loaders/FontLoader.js';
-// import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 
 var camera, scene, renderer, group = new THREE.Group();
 const container = document.createElement('div');
@@ -18,77 +13,6 @@ container.style.height = '100%';
 document.body.appendChild(container);
 var urlParams = getUrlParameters();
 
-urlParams.name = urlParams.name || '某某某'
-urlParams.time = urlParams.time || '9999.99.99 加入'
-urlParams.code = urlParams.code || 'H00000000'
-
-if(urlParams.lightIntensity) {
-    urlParams.lightIntensity = Number(urlParams.lightIntensity);
-} else {
-    urlParams.lightIntensity = 4
-}
-
-if(urlParams.cardMetalness) {
-    urlParams.cardMetalness = Number(urlParams.cardMetalness);
-} else {
-    urlParams.cardMetalness = 1
-}
-
-if(urlParams.cardRoughness) {
-    urlParams.cardRoughness = Number(urlParams.cardRoughness);
-} else {
-    urlParams.cardRoughness = 0.35
-}
-
-if(urlParams.cameraX) {
-    urlParams.cameraX = Number(urlParams.cameraX);
-} else {
-    urlParams.cameraX = -12
-}
-
-if(urlParams.cameraX) {
-    urlParams.cameraX = Number(urlParams.cameraX);
-} else {
-    urlParams.cameraX = -12
-}
-
-if(urlParams.cameraY) {
-    urlParams.cameraY = Number(urlParams.cameraY);
-} else {
-    urlParams.cameraY = 0
-}
-
-if(urlParams.cameraZ) {
-    urlParams.cameraZ = Number(urlParams.cameraZ);
-} else {
-    urlParams.cameraZ = 36
-}
-
-if(urlParams.fontColor) {
-    urlParams.fontColor = parseInt(urlParams.fontColor, 16);
-} else {
-    urlParams.fontColor = parseInt('0xffffff', 16);
-}
-
-if(urlParams.lightingColor) {
-    urlParams.lightingColor = parseInt(urlParams.lightingColor, 16);
-} else {
-    urlParams.lightingColor = parseInt('0xffff00', 16);
-}
-
-if(urlParams.cardColor) {
-    urlParams.cardColor = parseInt(urlParams.cardColor, 16);
-} else {
-    urlParams.cardColor = parseInt('0x2e0f7c', 16);
-}
-
-if(urlParams.backgroundColor) {
-    urlParams.backgroundColor = parseInt(urlParams.backgroundColor, 16);
-} else {
-    urlParams.backgroundColor = parseInt('0x0000ff', 16);
-    // urlParams.backgroundColor = parseInt('0x681D9A', 16);
-}
-
 function init() {
     scene = new THREE.Scene();
     scene.background = new THREE.Color( urlParams.backgroundColor );
@@ -96,8 +20,6 @@ function init() {
 
     group.castShadow = true;
     scene.add(group);
-
-    // setPlane(100, 100, urlParams.backgroundColor);
 
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     // 启用阴影贴图
@@ -121,20 +43,13 @@ function init() {
     controls.minPolarAngle = Math.PI / 4;
     controls.maxPolarAngle = Math.PI / 2 + Math.PI / 4;
 
-    var ambientLight = new THREE.AmbientLight(0xffffff, urlParams.lightIntensity * 100);
-    scene.add(ambientLight);
+    // var ambientLight = new THREE.AmbientLight(0xffffff, urlParams.lightIntensity * 100);
+    // scene.add(ambientLight);
 
-    // setDirectionalLight(0, 30, 0, urlParams.lightIntensity * 1);
     setDirectionalLight(0, 5, 100, urlParams.lightIntensity * 0.5);
     setDirectionalLight(0, 5, -100, urlParams.lightIntensity * 0.5);
-
     setDirectionalLight(100, 5, 0, urlParams.lightIntensity * 0.8);
     setDirectionalLight(-100, 5, 0, urlParams.lightIntensity * 0.8);
-
-    // setDirectionalLight(100, 5, 100, 0.01);
-    // setDirectionalLight(-100, 5, 100, 0.01);
-    // setDirectionalLight(100, 5, -100, 0.01);
-    // setDirectionalLight(-100, 5, -100, 0.01);
 
     let cardOptions = {
         width: 12,
@@ -143,28 +58,42 @@ function init() {
         radius: 1,
     }
 
-    let fontDepth = 0.1;
-
+    // 创建卡牌
     setCubeCard(cardOptions.width, cardOptions.height, cardOptions.depth, cardOptions.radius);
-    // setCubeCard2();
 
-    setText('光子之城', 0, 6.8, cardOptions.depth + 0.2, 0.8, urlParams.fontColor);
-    setText(urlParams.name, 0, 0, cardOptions.depth + 0.2, 1.5, urlParams.fontColor);
-    setText(urlParams.time, 0, -7, cardOptions.depth + 0.2, 0.6, urlParams.fontColor);
-    setText('光子市民', 0, -5.5, 0 - 0.2, 1, urlParams.fontColor, 0.5);
-    setText(urlParams.code, 0, -7, 0 - 0.2, 0.7, urlParams.fontColor, 0.5);
+    if(urlParams.isHeader) { // 是否展示头像
+        // 创建闪光模型
+        setFlashModel(5, -1.6, cardOptions.depth + 0.1, 0.7);
 
-    // setText2('光子之城', -1.6, 6, cardOptions.depth, 0.6, fontDepth, urlParams.fontColor);
-    // setText2(urlParams.name, -1.9, 0, cardOptions.depth, 1, fontDepth, urlParams.fontColor);
-    // setText2(urlParams.time + ' 加入', -2.4, -6, cardOptions.depth, 0.5, fontDepth, urlParams.fontColor);
-    // setText2('光子市民', 1.4, -4.5, 0, 0.5, fontDepth, urlParams.fontColor, 0.5);
-    // setText2(urlParams.code, 1.5, -6, 0, 0.5, fontDepth, urlParams.fontColor, 0.5);
+        // 正面
+        setImage(urlParams.header, 0, 3.5, cardOptions.depth + 0.15, 10.5, 10.5, 1);
+        setText(urlParams.name, -3.8, -2.5, cardOptions.depth + 0.15, 1, urlParams.fontColor);
+        setText(urlParams.description, 0, -5.5, cardOptions.depth + 0.15, 0.5, urlParams.fontColor, 0, 10.5);
+        setText(urlParams.author, -3.55, -8.5, cardOptions.depth + 0.15, 0.5, urlParams.fontColor);
 
-    setFlashModel(-1, 7, -0.2, 4);
-
+        // 背面
+        setText('光子之城', 0, 6.8, -0.15, 0.8, urlParams.fontColor, 0.5);
+        setText(urlParams.name, 0, 0, -0.15, 1.5, urlParams.fontColor, 0.5);
+        setText(urlParams.time, 0, -7, -0.15, 0.6, urlParams.fontColor, 0.5);
+    } else {
+        // 创建闪光模型
+        setFlashModel(-1, 7, -0.15, 4, 0.5);
+            
+        // 创建文字
+        // 正面
+        setText('光子之城', 0, 6.8, cardOptions.depth + 0.15, 0.8, urlParams.fontColor);
+        setText(urlParams.name, 0, 0, cardOptions.depth + 0.15, 1.5, urlParams.fontColor);
+        setText(urlParams.time, 0, -7, cardOptions.depth + 0.15, 0.6, urlParams.fontColor);
+        // 背面
+        setText('光子市民', 0, -5.5, -0.15, 1, urlParams.fontColor, 0.5);
+        setText(urlParams.code, 0, -7, -0.15, 0.7, urlParams.fontColor, 0.5);
+    }
+    
     window.addEventListener('resize', onResize, false);
     render();
 }
+
+window.onload = init;
 
 function render(time) {
     requestAnimationFrame(render);
@@ -176,30 +105,6 @@ function onResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-window.onload = init;
-
-function setCubeCard2() {
-    // 加载GLB模型
-    const loader = new GLTFLoader();
-    loader.load('model/id_card.glb', (gltf) => {
-        const model = gltf.scene;
-
-        group.add(model);
-
-        // model.position.set(x, y, z);
-        // model.scale.set(size, size, size);
-        // model.rotation.y = Math.PI;
-
-        // model.traverse(function (child) {
-        //     if (child.isMesh) {
-        //         child.material.roughness = 0.15;
-        //         child.material.color = new THREE.Color(urlParams.lightingColor);
-        //         child.material.emissive = new THREE.Color(urlParams.lightingColor);
-        //     }
-        // });
-    });
 }
 
 function setCubeCard(width, height, depth, radius) {
@@ -223,25 +128,6 @@ function setCubeCard(width, height, depth, radius) {
     let extrudeSettings = { depth: depth, bevelEnabled: false };
     let geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
 
-
-    // manager
-    // const manager = new THREE.LoadingManager( render );
-
-    // // matcap
-    // const loaderEXR = new EXRLoader( manager );
-    // const matcap = loaderEXR.load( 'model/040full.exr' );
-
-    // // normalmap
-    // const loader = new THREE.TextureLoader( manager );
-
-    // const normalmap = loader.load( 'model/face.jpg' );
-    // // 创建材质
-    // let material = new THREE.MeshMatcapMaterial( {
-    //     color: 0xffffff, 
-    //     matcap: matcap,
-    //     normalMap: normalmap
-    // } );
-
     let material = new THREE.MeshStandardMaterial({ 
         color: urlParams.cardColor, 
         metalness: urlParams.cardMetalness, 
@@ -250,9 +136,7 @@ function setCubeCard(width, height, depth, radius) {
 
     // 创建网格
     let card = new THREE.Mesh(geometry, material);
-    // card.castShadow = true;
-    // card.receiveShadow = true;
-
+    
     group.add(card);
 }
 
@@ -270,47 +154,78 @@ function setDirectionalLight(x, y, z, intensity){
     scene.add(directionalLight);
 }
 
-// function setPlane(width, height, color){
-//     // 创建平面几何体
-//     let planeGeometry = new THREE.PlaneGeometry(width, height);
-//     let planeMaterial = new THREE.MeshStandardMaterial({color: color});
-//     let plane = new THREE.Mesh(planeGeometry, planeMaterial);
-
-//     // 旋转并设置平面的位置
-//     plane.rotation.x = -0.5 * Math.PI;
-//     plane.position.y = -8.5;
-    
-//     // 平面应该接收阴影
-//     plane.receiveShadow = true;
-
-//     // 添加平面到场景中
-//     scene.add(plane);
-// }
-
-// function reverseModel() {
-//     // Define the final values for the rotation (additional 180 degrees)
-//     var finalRotationX = group.rotation.x; // + Math.PI;
-//     var finalRotationY = group.rotation.y + Math.PI;
-//     var finalRotationZ = group.rotation.z; // + Math.PI;
-
-//     // Create a new tween that modifies 'group.rotation'
-//     new TWEEN.Tween(group.rotation)
-//         .to({ x: finalRotationX, y: finalRotationY, z: finalRotationZ }, 500) // 2000 ms = 2 seconds
-//         .easing(TWEEN.Easing.Quadratic.InOut) // Use an easing function to make the animation smooth.
-//         .start(); // Start the tween immediately.
-// }
-
 // 获取url参数
 function getUrlParameters(url = window.location.href) {
     let params = new URLSearchParams((new URL(url)).search);
-    let paramsObject = {};
+    let paramsObject = {
+        name: '某某某',              // 姓名
+        time: '9999.99.99 加入',     // 加入时间
+        code: 'H00000000',          // 光子市民码
+        lightIntensity: 4,          // 光照强度
+        cardMetalness: 1,           // 卡片金属度
+        cardRoughness: 0.35,        // 卡片粗糙度
+        cameraX: -12,               // 相机位置x
+        cameraY: 0,                 // 相机位置y
+        cameraZ: 36,                // 相机位置z
+        fontColor: 0xffffff,        // 字体颜色
+        lightingColor: 0xffff00,    // 闪光颜色
+        cardColor: 0x2E0F7C,        // 卡片颜色
+        backgroundColor: 0x753EBC,  // 背景色
+
+        isHeader: false,  // 是否展示头像
+        header: 'https://download.mashaojie.cn/image/header.png',  // 头像地址
+        description: '我是一段介绍文字，总之很牛逼就对了！我是一段介绍文字，总之很牛逼就对了！我是一段介绍文字，总之很牛逼就对了！我是一段介绍文字，总之很牛逼就对了！我是一段介绍文字，总之很牛逼就对了！我是一段介绍文字，总之很牛逼就对了！我是一段介绍文字，总之很牛逼就对了！我是一段介绍文字，总之很牛逼就对了！',
+        author: '创建者@某某某',  // 作者
+    };
+
     for(let [key, value] of params.entries()) {
         paramsObject[key] = value;
     }
+
+    if(paramsObject.lightIntensity) {
+        paramsObject.lightIntensity = Number(paramsObject.lightIntensity);
+    } 
+    
+    if(paramsObject.cardMetalness) {
+        paramsObject.cardMetalness = Number(paramsObject.cardMetalness);
+    }
+
+    if(paramsObject.cardRoughness) {
+        paramsObject.cardRoughness = Number(paramsObject.cardRoughness);
+    }
+    
+    if(paramsObject.cameraX) {
+        paramsObject.cameraX = Number(paramsObject.cameraX);
+    }
+
+    if(paramsObject.cameraY) {
+        paramsObject.cameraY = Number(paramsObject.cameraY);
+    }
+
+    if(paramsObject.cameraZ) {
+        paramsObject.cameraZ = Number(paramsObject.cameraZ);
+    }
+
+    if(paramsObject.fontColor && typeof paramsObject.fontColor === 'string') {
+        paramsObject.fontColor = parseInt(paramsObject.fontColor, 16);
+    }
+
+    if(paramsObject.lightingColor && typeof paramsObject.lightingColor === 'string') {
+        paramsObject.lightingColor = parseInt(paramsObject.lightingColor, 16);
+    }
+
+    if(paramsObject.cardColor && typeof paramsObject.cardColor === 'string') {
+        paramsObject.cardColor = parseInt(paramsObject.cardColor, 16);
+    }
+
+    if(paramsObject.backgroundColor && typeof paramsObject.backgroundColor === 'string') {
+        paramsObject.backgroundColor = parseInt(paramsObject.backgroundColor, 16);
+    }
+
     return paramsObject;
 }
 
-function setFlashModel(x, y, z, size){
+function setFlashModel(x, y, z, size, rotationY = 0){
     // 加载GLB模型
     const loader = new GLTFLoader();
     loader.load('model/splash.glb', (gltf) => {
@@ -319,41 +234,45 @@ function setFlashModel(x, y, z, size){
 
         model.position.set(x, y, z);
         model.scale.set(size, size, size);
-        model.rotation.y = Math.PI;
+        model.rotation.y = rotationY * 2 * Math.PI;
 
         model.traverse(function (child) {
             if (child.isMesh) {
-                child.material.roughness = 0.15;
+                child.material.roughness = 0.01;
+                child.material.metalness = 1;
                 child.material.color = new THREE.Color(urlParams.lightingColor);
                 child.material.emissive = new THREE.Color(urlParams.lightingColor);
+                child.material.emissiveIntensity = 0.2;
             }
         });
     });
 }
 
-function setText(content, x, y, z, height, color, rotationY = 0) {
-    // 1. 创建一个新的div元素
+
+function setText(content, x, y, z, height, color, rotationY = 0, width = 0) {
     let domDiv = document.createElement('div');
 
-    let bigger = 100
-
-    // 2. 为div设置属性和内容
-    // domDiv.className = 'text-dom-class'  // 设置类名
+    let bigger = 100;  // 米单位到像素值的放大倍数
     domDiv.style.color = '#' + color.toString(16).padStart(6, '0');  // 设置绝对定位
-    domDiv.style.height = height * bigger + 'px'  // 设置绝对定位
-    domDiv.style.lineHeight = height * bigger + 'px'  // 设置绝对定位
-    domDiv.style.fontSize = height * bigger + 'px'  // 设置绝对定位
+    
     domDiv.style.position = 'absolute'  // 设置绝对定位
     domDiv.style.fontWeight = 'bold' // 设置绝对定位
     domDiv.style.backgroundColor = 'rgba(0, 0, 0, 0)' // 设置绝对定位
-
     domDiv.textContent = content;  // 设置内容
+
+    if(width) {
+        domDiv.style.width = width * bigger + 'px'  // 设置绝对定位
+        domDiv.style.textAlign = 'left'  // 设置绝对定位
+        domDiv.style.lineHeight = height * 1.2 * bigger + 'px'  // 设置绝对定位
+        domDiv.style.fontSize = height * bigger + 'px'  // 设置绝对定位
+    } else {
+        domDiv.style.height = height * bigger + 'px'  // 设置绝对定位
+        domDiv.style.lineHeight = height * bigger + 'px'  // 设置绝对定位
+        domDiv.style.fontSize = height * bigger + 'px'  // 设置绝对定位
+    }
 
     document.getElementById('html2canvas').appendChild(domDiv)
     
-    // domDiv = document.getElementById("text")
-
-
     setTimeout(() => {
         let widthPlane = domDiv.getBoundingClientRect().width || 1
         let heightPlane = domDiv.getBoundingClientRect().height || 1
@@ -365,7 +284,7 @@ function setText(content, x, y, z, height, color, rotationY = 0) {
             let texture = new THREE.CanvasTexture(canvas);
 
             let planeGeometry = new THREE.PlaneGeometry(widthPlane/bigger, heightPlane/bigger); 
-            let planeMaterial = new THREE.MeshBasicMaterial({ map: texture, transparent: true });
+            let planeMaterial = new THREE.MeshStandardMaterial({ map: texture, transparent: true, emissive: color });
 
             let textMesh = new THREE.Mesh(planeGeometry, planeMaterial);
             textMesh.position.x = x;
@@ -373,34 +292,62 @@ function setText(content, x, y, z, height, color, rotationY = 0) {
             textMesh.position.z = z;
 
             textMesh.rotation.y = rotationY * 2 * Math.PI;
+
             group.add(textMesh);
         })
     })
 }
 
-// function setText2(content, x, y, z, size, deep, color, rotationY = 0) {
-//     // 加载字体
-//     let fontLoader = new FontLoader();
-//     fontLoader.load('fonts/SugarCake_Regular.json', function(font) {
-//         // 创建文本几何
-//         let textGeometry = new TextGeometry(content, {
-//             font: font,
-//             size: size, // 字体大小
-//             height: deep, // 文本深度
-//             curveSegments: 7, // 曲线分段数，更高的值会增加准确度但降低性能
-//             bevelEnabled: false, // 不启用斜角
-//         });
-//         // 创建纯色文本材质
-//         let textMaterial = new THREE.MeshBasicMaterial({ color: color }); // 红色
-//         // 创建文本网格
-//         let textMesh = new THREE.Mesh(textGeometry, textMaterial);
-//         textMesh.position.x = x;
-//         textMesh.position.y = y;
-//         textMesh.position.z = z;
+function setImage(src, x, y, z, width, height, radius, rotationY = 0) {
+    const img = new Image();
+    img.crossOrigin = "anonymous"; // 尝试请求 CORS 权限
+    img.src = src
+    img.onload = function() {
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
 
-//         textMesh.rotation.y = rotationY * 2 * Math.PI;
+        // 设置canvas尺寸并绘制图像
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img, 0, 0, img.width, img.height);
 
-//         // 添加到场景
-//         group.add(textMesh);
-//     });
-// }
+        // 从canvas获取blob
+        canvas.toBlob(function(blob) {
+            // 从Blob对象创建一个URL
+            const url = URL.createObjectURL(blob);
+            let bigger = 100;  // 米单位到像素值的放大倍数
+            let domDiv = document.createElement('img');
+    
+            domDiv.src = url;
+            domDiv.style.height = height * bigger + 'px'  // 设置绝对定位
+            domDiv.style.width = width * bigger + 'px'  // 设置绝对定位
+            domDiv.style.borderRadius = radius * bigger + 'px'  // 设置绝对定位
+            domDiv.style.position = 'absolute'  // 设置绝对定位
+            document.getElementById('html2canvas').appendChild(domDiv)
+        
+            setTimeout(() => {
+                let widthPlane = domDiv.getBoundingClientRect().width || 1
+                let heightPlane = domDiv.getBoundingClientRect().height || 1
+
+                html2canvas(domDiv, {
+                    backgroundColor: 'rgba(0, 0, 0, 0)',
+                    allowTaint: false,
+                }).then(canvas => {
+                    let texture = new THREE.CanvasTexture(canvas);
+    
+                    let planeGeometry = new THREE.PlaneGeometry(widthPlane/bigger, heightPlane/bigger); 
+                    let planeMaterial = new THREE.MeshStandardMaterial({ map: texture, transparent: true });
+    
+                    let textMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+                    textMesh.position.x = x;
+                    textMesh.position.y = y;
+                    textMesh.position.z = z;
+    
+                    textMesh.rotation.y = rotationY * 2 * Math.PI;
+    
+                    group.add(textMesh);
+                })
+            })
+        }, 'image/png'); // 'image/png' 是输出的格式，可以根据需要更改
+    };
+}
